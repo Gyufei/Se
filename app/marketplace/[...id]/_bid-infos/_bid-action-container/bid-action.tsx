543d40a28068a050f32a5f68435c2747b37b8020
fei guo <812773673@qq.com>
2024-11-13 17:35:59 +0800

    feat:add-popover

--- app/marketplace/[...id]/_bid-infos/_bid-action-container/bid-action.tsx
+++ app/marketplace/[...id]/_bid-infos/_bid-action-container/bid-action.tsx
@@ -1,12 +1,38 @@
 import Image from "next/image";
 import { NumericalInput } from "@/components/share/numerical-input";
 import { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { truncateAddr } from "@/lib/utils/web3";
 
 const balance = 9.00655;
 const canBidAsPool = true;
+const pools = [
+  {
+    address: "0x1234567890123456789123456789123456789",
+  },
+  {
+    address: "0x000000000000000000000ffffffffffffffff",
+  },
+  {
+    address: "0x0000000000000000000000000000000000001",
+  },
+  {
+    address: "0x0000000000000000000000000000000000004",
+  },
+  {
+    address: "0x0000000000000000000000000000000000003",
+  },
+];
 
 export default function BidAction() {
   const [bidAmount, setBidAmount] = useState(0);
+  const [selectedPool, setSelectedPool] = useState<string | null>(null);
 
   const handleInput = (value: string) => {
     setBidAmount(Number(value));
@@ -53,11 +79,36 @@ export default function BidAction() {
         </div>
       </div>
 
-      <div className="flex items-center justify-between mt-5">
+      <div className="flex items-center justify-between mt-5 space-x-5">
         {canBidAsPool ? (
-          <button className="bg-green h-12 flex justify-center items-center flex-1 text-[#12021D] text-base font-bold">
-            Bid as
-          </button>
+          <>
+            <button className="bg-green h-12 flex justify-center items-center flex-1 text-[#12021D] text-base font-bold">
+              Bid as
+            </button>
+            <Select
+              value={selectedPool || ""}
+              onValueChange={(v) => setSelectedPool(v)}
+            >
+              <SelectTrigger className="flex-1 h-12 bg-[#382743] border-none rounded-none flex justify-center space-x-[5px]">
+                <SelectValue placeholder="Select a Pool">
+                  <span className="text-white text-base font-medium">
+                    Pool: {truncateAddr(selectedPool || "")}
+                  </span>
+                </SelectValue>
+              </SelectTrigger>
+              <SelectContent className="border-none rounded-none bg-[#382743] p-[5px]">
+                {pools.map((pool) => (
+                  <SelectItem
+                    className="text-white mt-[5px] text-base font-medium hover:bg-[#281A31] data-[state=checked]:bg-[#281A31]"
+                    key={pool.address}
+                    value={pool.address}
+                  >
+                    Pool: {truncateAddr(pool.address)}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </>
         ) : (
           <button className="bg-green h-12 flex justify-center items-center flex-1 text-[#12021D] text-base font-bold">
             Bid
